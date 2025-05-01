f08ae60379935590c3af5f343eedab62125d4fcc
Piotr Szyma <thompson2908@gmail.com>
2025-05-01 16:28:59 +0200

    feat: help

--- cmd/context.go
+++ cmd/context.go
@@ -9,12 +9,15 @@ import (
 var contextCmd = &cobra.Command{
 	Use:   "context",
 	Short: "Change context interactively",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Long: `The 'context' command provides an interactive way to view and switch your
+current Kubernetes context. It enhances the standard 'kubectl config use-context'
+by offering a fuzzy finder interface and support for custom aliases, making it
+easier to manage numerous or complex context names.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Configuration:
+To define custom aliases, create or edit the configuration file at:
+'~/.config/kk/config.yaml'
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cli.ChangeContext()
 	},
